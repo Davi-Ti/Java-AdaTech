b7460eb8aec72911d71b3403295d6a9e433f9b1b
Davi-Ti <davi.connect@outlook.com>
2025-03-09 09:53:03 -0300

    Implement basic arithmetic operations in Main.java

--- Introducao/hello/world/Main.java
+++ Introducao/hello/world/Main.java
@@ -3,8 +3,18 @@ package hello.world;
 public class Main {
     public static void main(String[] args) {
 
-        String nome = "Davi";
-        System.out.println("Ola!, " + nome);
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtracao: " + subtracao);
+        System.out.println("Multiplicacao: " + multiplicacao);
+        System.out.println("Divisao: " + divisao);
 
 
     }
