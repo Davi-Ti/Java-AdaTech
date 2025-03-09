701da0588f3a5f99a6c9a0213bf62c3a83a61990
Davi-Ti <davi.connect@outlook.com>
2025-03-09 12:56:55 -0300

    Add data class to display greeting and current day of the week

---
+++ Introducao/data.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.time.format.TextStyle;
+
+public class data {
+
+    public static void main(String[] args) {
+        String nome = "Davi";
+        
+        LocalDate hoje = LocalDate.now();
+        Locale brasil = Locale.forLanguageTag("pt-BR");
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        String saudacao;
+        LocalDateTime agora = LocalDateTime.now();
+        int hora = agora.getHour();
+        if (hora < 12) {
+            saudacao = "Bom dia";
+        } else if (hora < 18) {
+            saudacao = "Boa tarde";
+        } else {
+            saudacao = "Boa noite";
+        }
+
+        System.out.printf("Olá %s, hoje é %s, %s\n", nome, diaSemana, saudacao);
+
+    }
+
+}
