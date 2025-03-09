15c138d19dc284263c8aeaad634b4104a52532ef
Davi-Ti <davi.connect@outlook.com>
2025-03-09 12:56:50 -0300

    Add strings class to demonstrate string manipulation methods

---
+++ Introducao/strings.java
@@ -0,0 +1,22 @@
+public class strings {
+
+    public static void main(String[] args) {
+
+
+        String nome = "Davi";
+
+        System.out.println(nome.toUpperCase());
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.length());
+
+        String nomeOutro = "davi";
+        System.out.println(nome.equals(nomeOutro));
+        System.out.println(nome.equalsIgnoreCase(nomeOutro));
+
+
+
+    }
+
+
+
+}
