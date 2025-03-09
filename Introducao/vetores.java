f6c76ebaff772000fea4e6e045aa4775db69ce5f
Davi-Ti <davi.connect@outlook.com>
2025-03-09 14:50:53 -0300

    Add vetores class to demonstrate array usage and iteration in Java

---
+++ Introducao/vetores.java
@@ -0,0 +1,64 @@
+public class vetores {
+
+    public static void main(String[] args) {
+
+        // int[] numeros = new int[5];
+        // numeros[0] = 10;
+        // numeros[1] = 20;
+        // numeros[2] = 30;
+        // numeros[3] = 40;
+        // numeros[4] = 50;
+
+        // for (int i = 0; i < numeros.length; i++) {
+        //     System.out.println(numeros[i]);
+        // }
+
+        // int[] numeros = {10, 20, 30, 40, 50};
+
+        // for (int i = 0; i < numeros.length; i++) {
+        //     System.out.println(numeros[i]);
+        // }
+
+        // for (int numero : numeros) {
+        //     System.out.println(numero);
+        // }
+
+        // int[][] numeros = new int[2][2];
+        // numeros[0][0] = 10;
+        // numeros[0][1] = 20;
+        // numeros[1][0] = 30;
+        // numeros[1][1] = 40;
+
+        // for (int i = 0; i < numeros.length; i++) {
+        //     for (int j = 0; j < numeros[i].length; j++) {
+        //         System.out.println(numeros[i][j]);
+        //     }
+        // }
+
+        // int[][] numeros = {{10, 20}, {30, 40}};
+
+        // for (int i = 0; i < numeros.length; i++) {
+        //     for (int j = 0; j < numeros[i].length; j++) {
+        //         System.out.println(numeros[i][j]);
+        //     }
+        // }
+
+        // for (int[] linha : numeros) {
+        //     for (int numero : linha) {
+        //         System.out.println(numero);
+        //     }
+        // }
+
+        // int[][][] numeros = {{{10, 20}, {30, 40}}, {{50, 60}, {70, 80}}};
+
+        // for (int i = 0; i < numeros.length; i++) {
+        //     for (int j = 0; j < numeros[i].length; j++) {
+        //         for (int k = 0; k < numeros[i][j].length; k++) {
+        //
+        //             System.out.println(numeros[i][j][k]);
+        //         }
+        //     }
+        // }
+
+}
+}
