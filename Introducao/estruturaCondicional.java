385abe3fd7cc07918891b6e5ddced45b9cfada0c
Davi-Ti <davi.connect@outlook.com>
2025-03-09 10:18:01 -0300

    Add estruturaCondicional class to demonstrate conditional statements

---
+++ Introducao/estruturaCondicional.java
@@ -0,0 +1,45 @@
+public class estruturaCondicional {
+
+    public static void main(String[] args) {
+        int nota = 70;
+
+        if (nota >= 70) {
+            System.out.println("Aprovado!");
+        } else {
+            System.out.println("Reprovado!");
+        }
+
+        //A 80 B 70 C 60 D 0
+        String graduacao;
+
+        if (nota >= 80) {
+            graduacao = "A";
+        } else if (nota >= 70) {
+            graduacao = "B";
+        } else if (nota >= 60) {
+            graduacao = "C";
+        } else {
+            graduacao = "D";
+        }
+
+        switch (graduacao) {
+            case "A":
+                System.out.println("Parabéns!");
+                break;
+            case "B":
+                System.out.println("Quase lá!");
+                break;
+            case "C":
+                System.out.println("Estude mais!");
+                break;
+            case "D":
+                System.out.println("Reprovado!");
+                break;
+            default:
+                System.out.println("Nota inválida!");
+        }
+
+
+    }
+
+}
