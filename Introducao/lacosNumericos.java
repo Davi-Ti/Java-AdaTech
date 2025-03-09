e159f6e9e8e61b211caa11b0f479bdf2f678deb8
Davi-Ti <davi.connect@outlook.com>
2025-03-09 14:45:00 -0300

    Add lacosNumericos class to demonstrate various loop constructs in Java

---
+++ Introducao/lacosNumericos.java
@@ -0,0 +1,73 @@
+public class lacosNumericos {
+    public static void main(String[] args) {
+
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println(i);
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     if (i == 5) {
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     if (i == 5) {
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        // int i = 0;
+        // while (i < 10) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i < 10);
+
+        // for (int i = 0; i < 10; i++) {
+        //     for (int j = 0; j < 10; j++) {
+        //         System.out.println(i + " " + j);
+        //     }
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     for (int j = 0; j < 10; j++) {
+        //         if (j == 5) {
+        //             break;
+        //         }
+        //         System.out.println(i + " " + j);
+        //     }
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     for (int j = 0; j < 10; j++) {
+        //         if (j == 5) {
+        //             continue;
+        //         }
+        //         System.out.println(i + " " + j);
+        //     }
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     if (i == 5) {
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     if (i == 5) {
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // }
+
+}
+}
