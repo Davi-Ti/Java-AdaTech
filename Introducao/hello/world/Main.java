a55a83486aa416b6644a51473bb9bcb9e00a8900
Davi-Ti <davi.connect@outlook.com>
2025-03-08 23:04:13 -0300

    rafactored

--- Introducao/hello/world/Main.java
+++ Introducao/hello/world/Main.java
@@ -1,7 +1,7 @@
-package HellowWorld;
+package hello.world;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
     }
-}git
+}
